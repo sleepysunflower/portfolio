121ad9dd27175c22c5160f56709dc69a6e749e9f
sleepysunflower <ufojwk@gmail.com>
2025-11-05 16:49:20 -0500

    Updated portfolio intro

--- script.js
+++ script.js
@@ -6,15 +6,15 @@ document.addEventListener('DOMContentLoaded', () => {
 const projects = [
   {
     title: "3D Visualization of Old Montreal Landmarks",
-    intro: "An interactive 3D web map visualizing landmark buildings in Old Montreal. Users can explore historic architecture, switch between modern and vintage basemaps, and view curated popups for each site.",
-    keywords: ["historic mapping","heritage Old Montreal","3D web GIS","Mapbox GL JS"],
+    intro: "An interactive 3D web map of Old Montreal's landmark buildings. I implemented dual basemap blending (satellite and custom vector styles), 2D/3D pitch toggling, and interactive popups using GeoJSON polygon data. I customized the interface design to support historical storytelling.",
+    keywords: ["historic mapping","heritage Old Montreal","3D Map","Mapbox GL JS"],
     url: "https://sleepysunflower.github.io/Old_MTL/",
     thumb: "assets/thumbnails/old_mtl.png",
     alt: "3D map of Old Montreal landmarks"
   },
   {
     title: "Mono River Basin Flood Risk Analysis",
-    intro: "A geospatial analysis of flood risk in the Mono River Basin (Togo–Benin) using rainfall–runoff modeling, terrain capacity, and population exposure. The project visualizes monthly flood dynamics and composite risk zones through interactive QGIS-based web maps.",
+    intro: "A watershed-scale flood risk assessment of the Mono River Basin using a rainfall and populatione exposure implemented fully in QGIS. The project visualizes monthly flood dynamics and composite risk zones through interactive QGIS-based web maps.",
     keywords: ["Flood modeling","Hydrology","Spatial analysis","QGIS","Leaflet","Remote sensing","JavaScript"],
     url: "https://sleepysunflower.github.io/mono_flood/",
     thumb: "assets/thumbnails/mono_flood.png",
@@ -22,16 +22,16 @@ const projects = [
   },
   {
     title: "Montreal Trees",
-    intro: "An interactive web map visualizing Montreal’s urban forest dynamic. Users can explore living and felled trees, filter by planting years, and overlay livability metrics such as heat, noise, and air quality.",
-    keywords: ["Tree","Interactive webmap","PostgreSQL","MapLibre GL JS","JavaScript"],
+    intro: "An interactive web map visualizing Montreal’s urban forest dynamic. Users can explore living and felled trees, filter by planting years, and overlay livability metrics such as heat, noise, and air quality. I preprocessed and joined multiple open datasets using SQL and Python, and built custom modules for species cards, year filters, and a bilingual UI to enable detailed tree-level exploration that are unavailable on other sites.",
+    keywords: ["Tree","Interactive webmap","PostgreSQL","Python","MapLibre GL JS","JavaScript"],
     url: "https://sleepysunflower.github.io/tree-mtl/",
     thumb: "assets/thumbnails/tree_mtl.png",
     alt: "Montreal urban forest map"
   },
   {
     title: "Traffic Collisions",
-    intro: "An interactive web portal analyzing Montreal’s traffic collisions (2012–2021). It visualizes spatial and temporal patterns, predicts collision occurrence and severity using random forest models. It integrates maps, charts, and dashboards for accessible, data-driven urban safety insights.",
-    keywords: ["Transportation","Spatial analysis","Machine learning","Vite+React","TypeScript"],
+    intro: "An interactive web portal analyzing Montreal’s traffic collisions (2012–2021). It allows detailed spatial–temporal exploration of collision patterns, which integrates MapLibre GL for spatial visualization and DuckDB-WASM for in-browser data querying.   The Random Forest models, trained in Python, perform real-time predictions of collision occurrence and severity.",
+    keywords: ["Transportation","Spatial analysis","Machine learning","Python","Vite+React","Javascript","TypeScript"],
     url: "https://sleepysunflower.github.io/traffic_collisions",
     thumb: "assets/thumbnails/traffic_collisions.png",
     alt: "Traffic collisions dashboard"
